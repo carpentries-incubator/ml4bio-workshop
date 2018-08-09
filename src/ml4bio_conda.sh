6aa70d9fe89da56761fb68b5b54665ab705d2a59
Anthony Gitter <gitter@biostat.wisc.edu>
2018-08-09 06:53:22 -0500

    Update check for existing conda env

--- src/ml4bio_conda.sh
+++ src/ml4bio_conda.sh
@@ -4,11 +4,15 @@
 
 # check whether the user already installed the ml4bio conda environment
 # by trying to activate the environment
-if !(source activate ml4bio); then
+source activate ml4bio
+
+if [ $? -ne 0 ]; then
   echo creating ml4bio environment
   conda env create -f conda_env.yml
   source activate ml4bio
 fi
+
+conda info --envs
 
 # launch the GUI
 python ml4bio.py
